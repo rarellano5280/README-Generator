e5c35910093f3622833374c5fc345c47b1776bcb
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-10-18 15:13:01 -0600

    finished functions in gen markdown. Still not sure if badge is working pushing to git to see if it shows up

---
+++ Develop/README.md
@@ -0,0 +1,26 @@
+# README Generator 
+  
+  [![MIT License license](https://img.shields.io/badge/License-MIT License-yellow.svg)](https://choosealicense.com/licenses/mit/
+
+  ## Description
+  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'
+
+  ## Installation
+  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly 
+
+  ## Usage
+  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected 
+
+  ## Contribute
+  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution 
+
+  ## Test
+  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly 
+
+  ## License
+  The project is protected under the MIT License license.
+
+  ## Contact Me
+  GitHub: https://github.com/rarellano5280
+  Email: robert.arellano001@gmail.com
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,17 +4,42 @@ function renderLicenseBadge(license) {
   if (license == 'No license') {
   return ``;
   } else {
-  return `![Badge](https://img.shields.io/badge/License-${license}-success.svg)`
+  return `[![${license} license](https://img.shields.io/badge/License-${license}-yellow.svg)](${renderLicenseLink(license)}`
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+if (license === 'Apache License 2.0') {
+  return 'https://www.apache.org/licenses/LICENSE-2.0'
+}
+if (license === 'GNU License v3.0') {
+  return 'https://www.gnu.org/licenses/gpl-3.0.en.html'
+}
+if (license === 'MIT License') {
+  return 'https://choosealicense.com/licenses/mit/'
+}
+if (license === 'Boost Software License 1.0') {
+  return 'https://www.boost.org/LICENSE_1_0.txt'
+}
+if (license === 'Mozilla Public License'){
+  return 'https://www.boost.org/LICENSE_1_0.txt'
+}else{
+  return ``
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (!license) {
+  return ``;
+} else {
+  return `## License
+  The project is protected under the ${license} license.`
+}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -36,6 +61,8 @@ function generateMarkdown(data) {
 
   ## Test
   ${data.test}
+
+  ${renderLicenseSection(data.license)}
 
   ## Contact Me
   GitHub: https://github.com/${data.github}
